09e18d56ee19700bff73e94c9c017d7d18b82595
mfranzon <43796979+mfranzon@users.noreply.github.com>
2019-04-05 17:58:49 +0200

    update

--- strassen/strassen_prova.c
+++ strassen/strassen_prova.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "matrix.h"
+#include <stdbool.h>
 
 
 
@@ -38,6 +39,8 @@ void strassen_aux_prova(float **C, const size_t C_f_row, const size_t C_f_col,
                float **A, const size_t A_f_row, const size_t A_f_col,
                float **B, const size_t B_f_row, const size_t B_f_col,
                const size_t n)
+
+
 {
   if (n < (1<<6)) {
     naive_aux(C, C_f_row, C_f_col,
@@ -66,183 +69,344 @@ void strassen_aux_prova(float **C, const size_t C_f_row, const size_t C_f_col,
   const size_t BX2 = B_f_col + n2;
 
 
-
-  //create only 4 matrix used to store temporary results
-
   float ***D=(float ***)malloc(sizeof(float **)*4);
-  for (int i=0; i<4; i++) {
-    D[i] = allocate_matrix(n2, n2);
-  }
-
+        for (int i=0; i<4; i++) {
+              D[i] = allocate_matrix(n2, n2);
+            }
   //rephrase the alghoritm in order to avoid memory consuming
 
+
+  sub_matrix_blocks(C, C2X, CX2, //S7
+                    A,A1X,AX2,
+                    A,A2X,AX2,
+                    n2);
+
+  sum_matrix_blocks(C, C1X, CX2, //S8
+                    B,B2X,BX1,
+                    B,B2X,BX2,
+                    n2);
+
+  strassen_aux_prova(D[0], 0, 0, //P6
+             C, C2X, CX2, //S7
+             C, C1X, CX2, //S8
+             n2);
+
+
+  sum_matrix_blocks(C, C2X, CX2, //S2
+                   A,A1X,AX1,
+                   A,A1X,AX2,
+                   n2);
+
+  strassen_aux_prova(C, C1X, CX2, //P2
+               C, C2X, CX2, //S2
+               B, B2X, BX2,
+               n2);
+
+
+  sum_matrix_blocks(C, C2X, CX2, //S5
+                    A,A1X,AX1,
+                    A,A2X,AX2,
+                    n2);
+
+
+  sum_matrix_blocks(C, C2X, CX1, //S6
+                    B,B1X,BX1,
+                    B,B2X,BX2,
+                    n2);
+
+  strassen_aux_prova(D[1], 0, 0, // P5
+                        C, C2X, CX2, //S5
+                        C, C2X, CX1, //S6
+                        n2);
+
+
+  sub_matrix_blocks(C, C2X, CX2, //S4
+                    B,B2X,BX1,
+                    B,B1X,BX1,
+                    n2);
+
+  strassen_aux_prova(D[2], 0, 0, // P4 //M2XMX1
+               A, A2X, AX2,
+               C, C2X, CX2, //S4
+               n2);
+
+  sum_matrix_blocks(C, C1X, CX1,
+              D[1], 0, 0, //P5
+              D[2], 0, 0, //P4
+              n2);
+
+  sub_matrix_blocks(C, C1X, CX1,
+                  C, C1X, CX1,
+                  C, C1X, CX2, //P2
+                  n2);
+
+  sum_matrix_blocks(C, C1X, CX1,
+                  C, C1X, CX1,
+                  D[0], 0, 0, //P6
+                  n2);
+
+
+
+
+  sub_matrix_blocks(C, C2X, CX1, //S1
+                  B, B1X,BX2,
+                  B, B2X,BX2,
+                  n2);
+
+  strassen_aux_prova(C, C2X, CX2, //P1
+             A, A1X, AX1,
+             C, C2X, CX1, //S1
+             n2);
+
+
+      sum_matrix_blocks(C, C1X, CX2,
+                        C, C2X, CX2, //P1
+                        C, C1X, CX2, //P2
+                        n2);
+
+
+
+  sum_matrix_blocks(C, C2X, CX1, //S3
+                     A, A2X, AX1,
+                     A, A2X, AX2,
+                     n2);
+
+   strassen_aux_prova(D[0], 0, 0, //P3
+              C, C2X, CX1, //S3
+              B, B1X, BX1,
+              n2);
+
+
+       sum_matrix_blocks(C, C2X, CX1,
+                         D[0], 0, 0, //P3
+                         D[2], 0, 0, //P4
+                         n2);
+
+
+
+  // P1 + P5
+  sum_matrix_blocks(C, C2X, CX2,
+                  C, C2X, CX2, //P1
+                  D[1], 0, 0, //P5
+                  n2);
+
+  // (P5 + P1) - P3
+  sub_matrix_blocks(C, C2X, CX2,
+                  C, C2X, CX2,
+                  D[0], 0, 0, //P3
+                  n2);
+
+  
+  sub_matrix_blocks( D[0], 0, 0, //S9
+                    A,A1X,AX1,
+                    A,A2X,AX1,
+                    n2);
+
+   sum_matrix_blocks( D[1], 0, 0, //S10
+                    B,B1X,BX1,
+                    B,B1X,BX2,
+                    n2);
+
+   strassen_aux_prova(D[2], 0, 0, //P7
+              D[0], 0, 0, //S9
+              D[1], 0, 0, //S10
+              n2);
+
+  // ((P5 + P1) - P3) - P7
+  sub_matrix_blocks(C, C2X, CX2,
+                  C, C2X, CX2,
+                  D[2], 0, 0, //P7
+                  n2);
+
+
+
+
+/*
   // S1 = B12 - B22
-  sub_matrix_blocks(D[0],0,0,
+  sub_matrix_blocks(C, C2X, CX1,
                     B,B1X,BX2,
                     B,B2X,BX2,
                     n2);
 
   // P1 = A11 x S1
-  strassen_aux_prova(D[0],0,0,
+  strassen_aux_prova(D[2],0,0,
                A,A1X,AX1,
-               D[0],0,0,
+               C, C2X, CX1,
                n2);
+
+  // C12 = P1
+  sum_matrix_blocks(C, C1X, CX2,
+                    C, C1X, CX2,
+                    D[2],0,0,
+                    n2);
+
+  // C22 = P1
+  sum_matrix_blocks(C, C2X, CX2,
+                    C, C2X, CX2,
+                    D[2],0,0,
+                    n2);
+
 
   // S2 = A11 + A12
-  sum_matrix_blocks(D[1],0,0,
+  sum_matrix_blocks(C, C2X, CX1,
                     A,A1X,AX1,
                     A,A1X,AX2,
                     n2);
 
+// P2 = S2 x B22
+strassen_aux_prova(D[0], 0, 0,
+             C, C2X, CX1,
+             B, B2X, BX2,
+             n2);
 
-  // P2 = S2 x B22
-  strassen_aux_prova(D[1],0,0,
-               D[1],0,0,
-               B,B2X,BX2,
+//C11-P2
+sub_matrix_blocks(C, C1X, CX1,
+                  C, C1X, CX1,
+                  D[0], 0, 0,
+                  n2);
+
+//C12 + P2
+sub_matrix_blocks(C, C1X, CX2,
+                  C, C1X, CX2,
+                  D[0], 0, 0,
+                  n2);
+
+// S3 = A21 + A22
+sum_matrix_blocks(C, C2X, CX1,
+                  A,A2X,AX1,
+                  A,A2X,AX2,
+                  n2);
+
+// P3 = S3 x B11
+  strassen_aux_prova(D[1], 0, 0,
+               C, C2X, CX1,
+               B, B1X, BX1,
+              n2);
+
+// S4 = B21 - B11
+  sub_matrix_blocks(C, C2X, CX1,
+                    B,B2X,BX1,
+                    B,B1X,BX1,
+                    n2);
+
+// P4 = A22 x S4
+  strassen_aux_prova(D[3], 0, 0,
+               A, A2X, AX2,
+               C, C2X, CX1,
                n2);
 
-  // S3 = A21 + A22
+//C21= P3 + P4
+sum_matrix_blocks(C, C2X, CX1,
+                  D[1], 0, 0,
+                  D[3], 0, 0,
+                  n2);
+
+
+//C22+P3
+sub_matrix_blocks(C, C2X, CX2,
+                  C, C2X, CX2,
+                  D[1], 0, 0,
+                  n2);
+
+
+//C11+P4
+sum_matrix_blocks(C, C1X, CX1,
+                  C, C1X, CX1,
+                  D[3], 0, 0,
+                  n2);
+
+// S5 = A11 + A22
   sum_matrix_blocks(D[2],0,0,
-                    A,A2X,AX1,
-                    A,A2X,AX2,
-                    n2);
-
-
-
-  // P3 = S3 x B11
-  strassen_aux_prova(D[2],0,0,
-               D[2],0,0,
-               B,B1X,BX1,
-               n2);
-
-  // S4 = B21 - B11
-  sub_matrix_blocks(D[3],0,0,
-                    B,B2X,BX1,
-                    B,B1X,BX1,
-                    n2);
-
-  // P4 = A22 x S4
-  strassen_aux_prova(D[3],0,0,
-               A,A2X,AX2,
-               D[3],0,0,
-               n2);
-
-
-  // C12 = P1 + P2
-  sum_matrix_blocks(C,C1X,CX2,
-                    D[0],0,0,
-                    D[1],0,0,
-                    n2);
-
-  // C21 = P3 + P4
-  sum_matrix_blocks(C,C2X,CX1,
-                    D[2],0,0,
-                    D[3],0,0,
-                    n2);
-
-  // S5 = A11 + A22
-  sum_matrix_blocks(C,C1X,CX1,
                     A,A1X,AX1,
                     A,A2X,AX2,
                     n2);
 
-  // S6 = B11 + B22
-  sum_matrix_blocks(C,C2X,CX2,
+// S6 = B11 + B22
+  sum_matrix_blocks(D[0],0,0,
                     B,B1X,BX1,
                     B,B2X,BX2,
                     n2);
 
-  // P5 = S5 x S6
-  strassen_aux_prova(C,C2X,CX2,
-               C,C1X,CX1,
-               C,C2X,CX2,
+
+// P5 = S5 x S6
+  strassen_aux_prova(D[1], 0, 0,
+               D[2],0,0,
+               D[0],0,0,
                n2);
+//C11 + P5
+sum_matrix_blocks(C, C1X, CX1,
+                  C, C1X, CX1,
+                  D[1], 0, 0,
+                  n2);
+//C22 + P5
+sum_matrix_blocks(C, C2X, CX2,
+                  C, C2X, CX2,
+                  D[1], 0, 0,
+                  n2);
 
 
-  // C11 = (P5 + P4) - P2
-  sum_matrix_blocks(C,C1X,CX1,
-                    C,C2X,CX2,
-                    D[3],0,0,
-                    n2);
-
-  sub_matrix_blocks(C,C1X,CX1,
-                    C,C1X,CX1,
-                    D[1],0,0,
-                    n2);
-
-  // S7 = A12 - A22
+// S7 = A12 - A22
   sub_matrix_blocks(D[3],0,0,
                     A,A1X,AX2,
                     A,A2X,AX2,
                     n2);
-  // S8 = B21 + B22
-  sum_matrix_blocks(D[1],0,0,
+// S8 = B21 + B22
+  sum_matrix_blocks(D[2],0,0,
                     B,B2X,BX1,
                     B,B2X,BX2,
                     n2);
 
-  // P6 = S7 x S8
-  strassen_aux_prova(D[1],0,0,
+// P6 = S7 x S8
+  strassen_aux_prova(D[0], 0, 0,
                D[3],0,0,
-               D[1],0,0,
+               D[2],0,0,
                n2);
 
-  // C11 = C11 + P6
+
+//C11+P6
+sum_matrix_blocks(C, C1X, CX1,
+                  C, C1X, CX1,
+                  D[0], 0, 0,
+                  n2);
 
 
-  sum_matrix_blocks(C,C1X,CX1,
-                    C,C1X,CX1,
-                    D[1],0,0,
-                    n2);
-
-
-  // C22 = P5 + P1 - P3
-  sum_matrix_blocks(C,C2X,CX2,
-                    C,C2X,CX2,
-                    D[0],0,0,
-                    n2);
-
-  sub_matrix_blocks(C,C2X,CX2,
-                    C,C2X,CX2,
-                    D[2],0,0,
-                    n2);
-
-
-  // S9 = A11 - A21
+// S9 = A11 - A21
   sub_matrix_blocks(D[1],0,0,
                     A,A1X,AX1,
                     A,A2X,AX1,
                     n2);
 
- // S10 = B11 + B12
+// S10 = B11 + B12
   sum_matrix_blocks(D[3],0,0,
                     B,B1X,BX1,
                     B,B1X,BX2,
                     n2);
 
 
-  // P7 = S9 x S10
-  strassen_aux_prova(D[3],0,0,
+// P7 = S9 x S10
+  strassen_aux_prova(D[2], 0, 0,
                D[1],0,0,
                D[3],0,0,
                n2);
 
 
-// C22 = C22 - P7
-  sub_matrix_blocks(C,C2X,CX2,
-                    C,C2X,CX2,
-                    D[3],0,0,
-                    n2);
-
-  for (int i=0; i<4; i++) {
-    deallocate_matrix(D[i], n2);
-  }
-
+//C22-P7
+sub_matrix_blocks (C, C2X, CX2,
+                   C, C2X, CX2,
+                   D[2], 0, 0,
+                  n2);
+*/
+        for (int i=0; i<4; i++) {
+        deallocate_matrix(D[i], n2);
+      }
 }
 
 void strassen_prova(float **C,
       float **A, float **B, const size_t n)
 {
+
+
   strassen_aux_prova(C, 0, 0, A, 0, 0, B, 0, 0, n);
+
 
 }
